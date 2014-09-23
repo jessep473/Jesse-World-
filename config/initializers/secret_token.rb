ab99315a4ebb348df8363854c125555d1bcbe572
Zach Whelchel <zach.whelchel@gmail.com>
2014-09-22 20:22:23 -0400

    set up project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JesseWorld::Application.config.secret_token = 'fb47fe9a09254783fc7fa6b405278429ae2f2039831d4bb8840d4f3bf8bf1b28d2c284b36fe8efbef13b9b5299a2a362104a38fbb9ad7f3105f8a552a475e9d6'
